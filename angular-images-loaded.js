ddbc876858f48d917520d282c540b18be4b493e9
James Homer <jameshomer85@gmail.com>
2014-09-02 17:56:16 +0100

    events

--- angular-images-loaded.js
+++ angular-images-loaded.js
@@ -7,7 +7,17 @@ angular.module('angular-images-loaded', []).directive('imagesLoaded', ['$timeout
                 var events = scope.$eval(attrs.imagesLoaded);
 
                 $timeout(function() {
+                    scope.$emit('imagesLoaded:started', {
+                        scope: scope,
+                        element: element
+                    });
+
                     var imgLoad = imagesLoaded(element[0], function() {
+                        scope.$emit('imagesLoaded:loaded', {
+                            scope: scope,
+                            element: element
+                        });
+
                         element.removeClass('images-loaded images-loaded: ' + attrs.imagesLoaded + ';');
                     });
 
