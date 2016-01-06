dddd5293a14a4322ce54d83d98a5709d64ec3dae
James Homer <jameshomer85@gmail.com>
2016-01-06 17:48:53 +0000

    update deps add index.js for bundlers

--- angular-images-loaded.js
+++ angular-images-loaded.js
@@ -1,62 +1,59 @@
-angular.module('angular-images-loaded', []);
-
-angular.module('angular-images-loaded').constant('imagesLoaded', imagesLoaded);
-
-angular.module('angular-images-loaded').directive('imagesLoaded', ['$timeout', 'imagesLoaded',
-    function($timeout, imagesLoaded) {
-        'use strict';
+;(function () {
+  angular.module('hj.imagesLoaded', [])
+    .directive('imagesLoaded', ['$timeout',
+      function ($timeout) {
+        'use strict'
         return {
-            restrict: 'AC',
-            link: function(scope, element, attrs) {
-                var events = scope.$eval(attrs.imagesLoaded) || scope.$eval(attrs.imagesLoadedEvents),
-                    className = attrs.imagesLoadedClass || 'images-loaded',
-                    classUsed = element.hasClass(className);
+          restrict: 'AC',
+          link: function (scope, element, attrs) {
+            var events = scope.$eval(attrs.imagesLoaded) || scope.$eval(attrs.imagesLoadedEvents)
+            var className = attrs.imagesLoadedClass || 'images-loaded'
+            var classUsed = element.hasClass(className)
 
-                var init = function() {
-                    $timeout(function() {
-                        scope.$imagesLoaded = false;
+            scope.$imagesLoaded = false
 
-                        scope.$emit('imagesLoaded:started', {
-                            scope: scope,
-                            element: element
-                        });
+            var init = function () {
+              scope.$emit('imagesLoaded:started', {
+                scope: scope,
+                element: element,
+              })
 
-                        if (classUsed) {
-                            element.addClass(className);
-                        }
+              if (classUsed) {
+                element.addClass(className)
+              }
 
-                        var imgLoad = imagesLoaded(element[0], function() {
-                            scope.$imagesLoaded = true;
+              var imgLoad = imagesLoaded(element[0], function () {
+                scope.$emit('imagesLoaded:loaded', {
+                  scope: scope,
+                  element: element,
+                })
 
-                            scope.$emit('imagesLoaded:loaded', {
-                                scope: scope,
-                                element: element
-                            });
+                element.removeClass(className + ' images-loaded: ' + attrs.imagesLoaded + ';')
 
-                            element.removeClass(className + ' images-loaded: ' + attrs.imagesLoaded + ';');
+                $timeout(function () {
+                  scope.$imagesLoaded = true
+                })
+              })
 
-                            if (!scope.$$phase) {
-                                scope.$apply();
-                            }
-                        });
-
-                        if (typeof(events) !== undefined) {
-                            angular.forEach(events, function(fn, eventName) {
-                                imgLoad.on(eventName, fn);
-                            });
-                        }
-                    });
-                };
-
-                if (attrs.imagesLoadedWatch) {
-                    scope.$watch(attrs.imagesLoadedWatch, function(n) {
-                        init();
-                    });
-
-                } else {
-                    init();
-                }
+              if (typeof events !== 'undefined') {
+                angular.forEach(events, function (fn, eventName) {
+                  imgLoad.on(eventName, fn)
+                })
+              }
             }
-        };
-    }
-]);
+
+            if (attrs.imagesLoadedWatch) {
+              scope.$watch(attrs.imagesLoadedWatch, function () {
+                $timeout(init)
+              })
+            } else {
+              $timeout(init)
+            }
+
+          }
+        }
+      }
+    ])
+
+  angular.module('angular-images-loaded', ['hj.imagesLoaded'])
+})()
---
+++ index.js
@@ -0,0 +1,7 @@
+var imagesLoaded = require('imagesloaded')
+if (window) {
+  window.imagesLoaded = imagesLoaded;
+}
+require('angular')
+require('./angular-images-loaded')
+module.exports = 'hj.imagesLoaded'
