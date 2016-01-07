5e0799f36aeeb5e75c45be48e79478ae4a8690e5
James Homer <jameshomer85@gmail.com>
2016-01-07 15:49:18 +0000

    put semi colons back

--- angular-images-loaded.js
+++ angular-images-loaded.js
@@ -1,59 +1,59 @@
-;(function () {
+(function () {
   angular.module('hj.imagesLoaded', [])
     .directive('imagesLoaded', ['$timeout',
       function ($timeout) {
-        'use strict'
+        'use strict';
         return {
           restrict: 'AC',
           link: function (scope, element, attrs) {
-            var events = scope.$eval(attrs.imagesLoaded) || scope.$eval(attrs.imagesLoadedEvents)
-            var className = attrs.imagesLoadedClass || 'images-loaded'
-            var classUsed = element.hasClass(className)
+            var events = scope.$eval(attrs.imagesLoaded) || scope.$eval(attrs.imagesLoadedEvents);
+            var className = attrs.imagesLoadedClass || 'images-loaded';
+            var classUsed = element.hasClass(className);
 
-            scope.$imagesLoaded = false
+            scope.$imagesLoaded = false;
 
             var init = function () {
               scope.$emit('imagesLoaded:started', {
                 scope: scope,
                 element: element,
-              })
+              });
 
               if (classUsed) {
-                element.addClass(className)
+                element.addClass(className);
               }
 
               var imgLoad = imagesLoaded(element[0], function () {
                 scope.$emit('imagesLoaded:loaded', {
                   scope: scope,
                   element: element,
-                })
+                });
 
-                element.removeClass(className + ' images-loaded: ' + attrs.imagesLoaded + ';')
+                element.removeClass(className + ' images-loaded: ' + attrs.imagesLoaded + ';');
 
                 $timeout(function () {
-                  scope.$imagesLoaded = true
-                })
-              })
+                  scope.$imagesLoaded = true;
+                });
+              });
 
               if (typeof events !== 'undefined') {
                 angular.forEach(events, function (fn, eventName) {
-                  imgLoad.on(eventName, fn)
-                })
+                  imgLoad.on(eventName, fn);
+                });
               }
-            }
+            };
 
             if (attrs.imagesLoadedWatch) {
               scope.$watch(attrs.imagesLoadedWatch, function () {
-                $timeout(init)
-              })
+                $timeout(init);
+              });
             } else {
-              $timeout(init)
+              $timeout(init);
             }
 
           }
-        }
+        };
       }
-    ])
+    ]);
 
-  angular.module('angular-images-loaded', ['hj.imagesLoaded'])
-})()
+  angular.module('angular-images-loaded', ['hj.imagesLoaded']);
+})();
